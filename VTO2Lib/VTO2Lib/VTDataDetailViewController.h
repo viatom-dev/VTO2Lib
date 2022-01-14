d6ad99975d3796edb558114873ee823dbfdbb300
yangweichao <yangweichao@viatomtech.com>
2022-01-14 21:09:04 +0800

    perf: displayed VTO2Object's parameters. fix: Modify a bug.

---
+++ VTO2Lib/VTO2Lib/VTDataDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  VTDataDetailViewController.h
+//  VTO2Lib
+//
+//  Created by yangweichao on 2022/1/14.
+//  Copyright © 2022 viatom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VTDataDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ VTO2Lib/VTO2Lib/VTDataDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  VTDataDetailViewController.m
+//  VTO2Lib
+//
+//  Created by yangweichao on 2022/1/14.
+//  Copyright © 2022 viatom. All rights reserved.
+//
+
+#import "VTDataDetailViewController.h"
+
+@interface VTDataDetailViewController ()
+
+@end
+
+@implementation VTDataDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
